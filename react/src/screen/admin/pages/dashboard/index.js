7e395c93c8abb366b3e83f111c7b49e66b44194d
Darvin <nagacoder@outlook.com>
2020-05-14 21:31:01 +0700

    Add | Favorite book and ebook list in dashboard

--- react/src/screen/admin/pages/dashboard/index.js
+++ react/src/screen/admin/pages/dashboard/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Table from '../../component/Table'
 import TopBar from '../../component/TopBar'
+import FavoriteBookAndEbookList from '../../component/FavoriteBookAndEbookList'
 function Dashboard() {
 
   return (
@@ -13,25 +14,7 @@ function Dashboard() {
           <main class="w-full flex-grow p-6">
             <h1 class="text-3xl text-black pb-6">Dashboard</h1>
             <TopBar />
-            <div class="flex flex-wrap mt-6">
-              <div class="w-full lg:w-1/2 pr-0 lg:pr-2">
-                <p class="text-xl pb-3 flex items-center">
-                  <i class="fas fa-plus mr-3"></i> Populer
-                        </p>
-                <div class="p-6 bg-white">
-                  <canvas id="chartOne" width="400" height="200"></canvas>
-                </div>
-              </div>
-              <div class="w-full lg:w-1/2 pl-0 lg:pl-2 mt-12 lg:mt-0">
-                <p class="text-xl pb-3 flex items-center">
-                  <i class="fas fa-check mr-3"></i> Pengunjung
-                        </p>
-                <div class="p-6 bg-white">
-                  <canvas id="chartTwo" width="400" height="200"></canvas>
-                </div>
-              </div>
-            </div>
-
+            <FavoriteBookAndEbookList />
             <div class="w-full mt-12">
               <p class="text-xl pb-3 flex items-center">
                 <i class="fas fa-list mr-3"></i> Daftar Buku
