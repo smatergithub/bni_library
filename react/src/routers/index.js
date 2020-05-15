897624736ffe8c23af588709078af59aac2cddc6
Darvin <nagacoder@outlook.com>
2020-05-16 03:11:02 +0700

    Add | create new repository form

--- react/src/routers/index.js
+++ react/src/routers/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../screen/admin/pages';
+import CreateNewRepo from '../screen/client/createNewRepo';
 
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/admin/:id" component={Dashboard} />
+        <Route exact path="/buat-repo-baru" component={CreateNewRepo} />
       </Switch>
     </Router>
   );
