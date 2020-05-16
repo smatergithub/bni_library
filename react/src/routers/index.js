76fe03ea781798d41b7621216a05dd58212107c8
Darvin <nagacoder@outlook.com>
2020-05-16 15:44:09 +0700

    Add | forgot password pages

--- react/src/routers/index.js
+++ react/src/routers/index.js
@@ -5,6 +5,7 @@ import Dashboard from '../screen/admin/pages';
 import CreateNewRepo from '../screen/client/createNewRepo';
 import Login from '../screen/client/login';
 import Register from '../screen/client/register';
+import ForgotPassword from '../screen/client/forgotPassword';
 
 function Routes() {
   return (
@@ -14,6 +15,7 @@ function Routes() {
         <Route exact path="/buat-repo-baru" component={CreateNewRepo} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/lupa-password" component={ForgotPassword} />
       </Switch>
     </Router>
   );
