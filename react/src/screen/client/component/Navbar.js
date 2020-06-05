b97e575df96c921d0dce59173cf58d22a2c85b1b
Darvin <nagacoder@outlook.com>
2020-06-05 07:41:57 +0700

    Add | about pages

--- react/src/screen/client/component/Navbar.js
+++ react/src/screen/client/component/Navbar.js
@@ -31,7 +31,13 @@ const routes = [
 function NavBar(url) {
   const [selectedMenu, setSelectedMenu] = useState(url);
   return (
-    <nav id="header" className="fixed w-full z-30 top-0 text-white">
+    <nav
+      id="header"
+      className="fixed w-full z-30 top-0 text-white"
+      style={{
+        zIndex: 1000,
+      }}
+    >
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div className="pl-4 flex items-center">
           <a
@@ -56,7 +62,7 @@ function NavBar(url) {
                 d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
               />
             </svg>{' '}
-            LANDING
+            BNI
           </a>
         </div>
 
