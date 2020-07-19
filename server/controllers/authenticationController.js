899af2cfed91f7a2ead639c2c2225dd62ef7d005
Darvin <nagacoder@outlook.com>
2020-07-19 10:23:53 +0700

    Update | return user role when login successfully

--- server/controllers/authenticationController.js
+++ server/controllers/authenticationController.js
@@ -9,7 +9,6 @@ var bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 module.exports = {
-
   register: async (req, res) => {
     Users.create({
       nama: req.body.nama,
@@ -18,7 +17,7 @@ module.exports = {
       password: bcrypt.hashSync(req.body.password, 8),
       isVerified: false,
       isAdmin: false,
-      superAdmin: false
+      superAdmin: false,
     })
       .then(user => {
         var expireDate = new Date();
@@ -26,17 +25,17 @@ module.exports = {
         VerificationToken.create({
           userId: user.id,
           token: cryptoRandomString({ length: 20 }),
-          expiredDateToken: expireDate
+          expiredDateToken: expireDate,
         })
           .then(verification => {
             res.status(203).send({
               message: 'account was registered successfully!',
               verificationToken: verification.token,
-              expiredDateToken: verification.expiredDateToken
+              expiredDateToken: verification.expiredDateToken,
             });
           })
           .catch(err => {
-            res.status(404).send({ message: "failed registered account" });
+            res.status(404).send({ message: 'failed registered account' });
           });
       })
       .catch(err => {
@@ -50,39 +49,36 @@ module.exports = {
     })
       .then(user => {
         if (user.isVerified) {
-          return res.status(404).json({ message: "Email Already verified" });
+          return res.status(404).json({ message: 'Email Already verified' });
         }
 
         VerificationToken.destroy({
           where: {
-            expiredDateToken: { [Op.lt]: Sequelize.fn('CURDATE') }
-          }
-        })
+            expiredDateToken: { [Op.lt]: Sequelize.fn('CURDATE') },
+          },
+        });
 
         var recordToken = VerificationToken.findOne({
-          where: { token: req.query.token, userId: user.id }
-        })
+          where: { token: req.query.token, userId: user.id },
+        });
 
         if (recordToken) {
-          user.update({ isVerified: true })
+          user
+            .update({ isVerified: true })
             .then(updateUser => {
               return res.status(200).json({ message: `User with ${user.email} has been verified` });
             })
             .catch(err => {
-              return res.status(404).json({ message: "Verification failed" });
+              return res.status(404).json({ message: 'Verification failed' });
             });
+        } else {
+          return res.status(404).json({ message: 'Token expired ' });
         }
-        else {
-          return res.status(404).json({ message: "Token expired " });
-        }
-
-
       })
       .catch(err => {
         return res.status(500).json({ message: err });
       });
   },
-
 
   login: async (req, res) => {
     Users.findOne({
@@ -94,7 +90,6 @@ module.exports = {
         if (!user) {
           return res.status(404).send({ message: 'User Not found.' });
         }
-
 
         var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
 
@@ -118,6 +113,7 @@ module.exports = {
         res.status(200).send({
           email: user.email,
           accessToken: token,
+          role: user.superAdmin === 1 ? '3' : user.isAdmin === 1 ? '2' : '1',
         });
       })
       .catch(err => {
@@ -125,38 +121,36 @@ module.exports = {
       });
   },
 
-
   resetPassword: async (req, res) => {
     await Users.findOne({
       where: {
-        email: req.body.email
-      }
-    })
-      .then(user => {
-        console.log("user", user);
-        VerificationToken.findOne({
-          where: {
-            userId: user.id
-          }
-        })
-          .then(verificationToken => {
-            var expireDate = new Date();
-            expireDate.setDate(expireDate.getDate() + 1 / 24);
-            verificationToken.update({
-              resetToken: cryptoRandomString({ length: 20 }),
-              expiredDateResetToken: expireDate
-            }).then(response => {
-              res.status(200).json({
-                resetToken: response.resetToken,
-                expiredDateResetToken: response.expiredDateResetToken
-              })
-            })
-              .catch(err => {
-                res.status(500).send({ message: err.message });
-              })
+        email: req.body.email,
+      },
+    }).then(user => {
+      console.log('user', user);
+      VerificationToken.findOne({
+        where: {
+          userId: user.id,
+        },
+      }).then(verificationToken => {
+        var expireDate = new Date();
+        expireDate.setDate(expireDate.getDate() + 1 / 24);
+        verificationToken
+          .update({
+            resetToken: cryptoRandomString({ length: 20 }),
+            expiredDateResetToken: expireDate,
           })
-
-      })
+          .then(response => {
+            res.status(200).json({
+              resetToken: response.resetToken,
+              expiredDateResetToken: response.expiredDateResetToken,
+            });
+          })
+          .catch(err => {
+            res.status(500).send({ message: err.message });
+          });
+      });
+    });
   },
 
   updatePassword: async (req, res) => {
@@ -166,37 +160,36 @@ module.exports = {
       .then(user => {
         VerificationToken.destroy({
           where: {
-            expiredDateResetToken: { [Op.lt]: Sequelize.fn('CURDATE') }
-          }
-        })
+            expiredDateResetToken: { [Op.lt]: Sequelize.fn('CURDATE') },
+          },
+        });
 
         var recordResetToken = VerificationToken.findOne({
           where: {
-            resetToken: req.query.resetToken, userId: user.id
-          }
-        })
+            resetToken: req.query.resetToken,
+            userId: user.id,
+          },
+        });
 
         if (recordResetToken) {
-
           if (req.body.confirmPassword !== req.body.password) {
             return res.status(404).json({ message: 'Passwords do not match. Please try again.' });
           }
-          user.update({
-            password: bcrypt.hashSync(req.body.password, 8)
-          },
+          user.update(
+            {
+              password: bcrypt.hashSync(req.body.password, 8),
+            },
             {
               where: {
-                email: req.query.email
-              }
-            })
-          return res.status(200).json({ message: "Succesfully Update Password" })
+                email: req.query.email,
+              },
+            }
+          );
+          return res.status(200).json({ message: 'Succesfully Update Password' });
         }
       })
       .catch(err => {
-        return res.status(500).json({ message: err })
-      })
-
-  }
+        return res.status(500).json({ message: err });
+      });
+  },
 };
-
-
