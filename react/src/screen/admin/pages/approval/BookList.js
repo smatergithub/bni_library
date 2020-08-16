9a79086644814002eb1879716df06b1871e12516
Darvin <nagacoder@outlook.com>
2020-08-16 16:06:28 +0700

    Finish Transaction return book/ebook from admin side

--- react/src/screen/admin/pages/approval/BookList.js
+++ react/src/screen/admin/pages/approval/BookList.js
@@ -1,132 +1,142 @@
 import React, { useState } from 'react';
-import { Modal } from '../../../../component';
-import TableApproval from "../../component/TableApproval";
 import { connect } from 'react-redux';
-import {
-  ListTransactionBook,
-  MakeReturnBook,
-} from "../../../../redux/action/transaction"
+import { NoData } from '../../../../component';
+import { ToastSuccess, ToastError } from '../../../../component';
+import TableApproval from '../../component/TableApproval';
 
-
-
+import { ListTransactionBook, MakeReturnBook } from '../../../../redux/action/transaction';
 
 function BookList(props) {
   const [loading, setLoading] = React.useState(false);
   const [detailData, setDetailData] = useState({});
   const [filterOptions, setFilterOptions] = React.useState({
     page: 1,
-    limit: 5
-  })
+    limit: 5,
+  });
 
-  const mappingDataSourceTransactionBookList = (filterOptions) => {
+  const mappingDataSourceTransactionBookList = filterOptions => {
     setLoading(true);
-    props.ListTransactionBook(filterOptions).then(res => {
-      if (res) {
-        setLoading(false);
-      }
-    }).catch(err => { console.log("error", err) });
-  }
+    props
+      .ListTransactionBook(filterOptions)
+      .then(res => {
+        if (res) {
+          setLoading(false);
+        }
+      })
+      .catch(err => {
+        console.log('error', err);
+      });
+  };
 
   React.useEffect(() => {
     mappingDataSourceTransactionBookList(filterOptions);
   }, []);
 
-  const onPaginationUpdated = (pagination) => {
+  const onPaginationUpdated = pagination => {
     setFilterOptions({
       page: pagination.page,
       limit: pagination.limit,
-    })
+    });
+  };
+  function returnBook(id) {
+    props.MakeReturnBook(id).then(res => {
+      if (res.resp) {
+        setLoading(false);
+        mappingDataSourceTransactionBookList(filterOptions);
+        ToastSuccess(res.msg);
+      } else {
+        setLoading(false);
+        ToastError(res.msg);
+      }
+    });
   }
 
   React.useEffect(() => {
     mappingDataSourceTransactionBookList(filterOptions);
-
-  }, [filterOptions])
-
+  }, [filterOptions]);
 
   const columns = [
     {
-      name: "code",
-      displayName: "Code"
+      name: 'code',
+      displayName: 'Code',
     },
     {
-      name: "judul",
-      displayName: "Judul",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.book.judul}
-          </React.Fragment>
-        );
+      name: 'judul',
+      displayName: 'Judul',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.book.judul}</React.Fragment>;
       },
     },
     {
-      name: "tahunTerbit",
-      displayName: "Tahun Terbit",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.book.tahunTerbit}
-          </React.Fragment>
-        );
+      name: 'tahunTerbit',
+      displayName: 'Tahun Terbit',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.book.tahunTerbit}</React.Fragment>;
       },
     },
 
     {
-      name: "nama",
-      displayName: "Peminjam",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.user.nama}
-          </React.Fragment>
-        );
+      name: 'nama',
+      displayName: 'Peminjam',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.user.nama}</React.Fragment>;
       },
     },
     {
-      name: "quantity",
-      displayName: "Jumlah Dipinjam",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.quantity}
-          </React.Fragment>
-        );
+      name: 'quantity',
+      displayName: 'Jumlah Dipinjam',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.quantity}</React.Fragment>;
       },
     },
     {
-      name: "status",
-      displayName: "Status"
+      name: 'status',
+      displayName: 'Status',
     },
     {
-      name: "actions",
-      displayName: "Actions",
-      customRender: (rowData) => {
+      name: 'actions',
+      displayName: 'Actions',
+      customRender: rowData => {
         return (
           <React.Fragment>
             <React.Fragment>
-              <button
-                className="bg-green-400 text-white active:bg-indigo-600 text-xs   px-3 py-1 rounded outline-none focus:outline-none "
-                type="button"
-                style={{ marginRight: '5px' }}
-                disabled={rowData.status === "Dikembalikan" ? true : false}
-              >
-                Return Book
-              </button>
+              {rowData.status !== 'Dikembalikan' ? (
+                <button
+                  className="bg-green-400 text-white active:bg-indigo-600 text-xs   px-3 py-1 rounded outline-none focus:outline-none "
+                  type="button"
+                  style={{ marginRight: '5px' }}
+                  onClick={() => returnBook(rowData.id)}
+                  disabled={rowData.status === 'Dikembalikan' ? true : false}
+                >
+                  Return Book
+                </button>
+              ) : (
+                '-'
+              )}
             </React.Fragment>
           </React.Fragment>
         );
       },
-    }
-  ]
-
+    },
+  ];
 
   if (loading) return null;
   const { transactionBooks } = props;
-
+  console.log(transactionBooks);
   return (
     <React.Fragment>
-      {transactionBooks.data !== undefined ? <TableApproval columns={columns} source={transactionBooks} isLoading={loading} limit={filterOptions.limit} page={filterOptions.page} onPaginationUpdated={onPaginationUpdated} /> : null}
+      {transactionBooks.data !== undefined && transactionBooks.data.length !== 0 ? (
+        <TableApproval
+          columns={columns}
+          source={transactionBooks}
+          isLoading={loading}
+          limit={filterOptions.limit}
+          page={filterOptions.page}
+          onPaginationUpdated={onPaginationUpdated}
+        />
+      ) : (
+        <NoData msg="Belum ada request dari user!" />
+      )}
     </React.Fragment>
   );
 }
--- react/src/screen/admin/pages/approval/EbooklList.js
+++ react/src/screen/admin/pages/approval/EbooklList.js
@@ -1,132 +1,141 @@
 import React, { useState } from 'react';
-import { Modal } from '../../../../component';
-import TableApproval from "../../component/TableApproval";
+import { NoData } from '../../../../component';
+import TableApproval from '../../component/TableApproval';
 import { connect } from 'react-redux';
-import {
-  MakeReturnEbook,
-  ListTransactionEbook
-} from "../../../../redux/action/transaction"
-
-
-
+import { ToastSuccess, ToastError } from '../../../../component';
+import { MakeReturnEbook, ListTransactionEbook } from '../../../../redux/action/transaction';
 
 function EbookList(props) {
   const [loading, setLoading] = React.useState(false);
   const [detailData, setDetailData] = useState({});
   const [filterOptions, setFilterOptions] = React.useState({
     page: 1,
-    limit: 5
-  })
+    limit: 5,
+  });
 
-  const mappingDataSourceTransactionEbookList = (filterOptions) => {
+  const mappingDataSourceTransactionEbookList = filterOptions => {
     setLoading(true);
-    props.ListTransactionEbook(filterOptions).then(res => {
-      if (res) {
-        setLoading(false);
-      }
-    }).catch(err => { console.log("error", err) });
-  }
+    props
+      .ListTransactionEbook(filterOptions)
+      .then(res => {
+        if (res) {
+          setLoading(false);
+        }
+      })
+      .catch(err => {
+        console.log('error', err);
+      });
+  };
 
   React.useEffect(() => {
     mappingDataSourceTransactionEbookList(filterOptions);
   }, []);
 
-  const onPaginationUpdated = (pagination) => {
+  const onPaginationUpdated = pagination => {
     setFilterOptions({
       page: pagination.page,
       limit: pagination.limit,
-    })
-  }
+    });
+  };
 
   React.useEffect(() => {
     mappingDataSourceTransactionEbookList(filterOptions);
-
-  }, [filterOptions])
-
+  }, [filterOptions]);
+  function returnEbook(id) {
+    props.MakeReturnEbook(id).then(res => {
+      if (res.resp) {
+        setLoading(false);
+        mappingDataSourceTransactionEbookList(filterOptions);
+        ToastSuccess(res.msg);
+      } else {
+        setLoading(false);
+        ToastError(res.msg);
+      }
+    });
+  }
 
   const columns = [
     {
-      name: "code",
-      displayName: "Code"
+      name: 'code',
+      displayName: 'Code',
     },
     {
-      name: "judul",
-      displayName: "Judul",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.book.judul}
-          </React.Fragment>
-        );
+      name: 'judul',
+      displayName: 'Judul',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.ebook.judul}</React.Fragment>;
       },
     },
     {
-      name: "tahunTerbit",
-      displayName: "Tahun Terbit",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.book.tahunTerbit}
-          </React.Fragment>
-        );
+      name: 'tahunTerbit',
+      displayName: 'Tahun Terbit',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.ebook.tahunTerbit}</React.Fragment>;
       },
     },
 
     {
-      name: "nama",
-      displayName: "Peminjam",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.user.nama}
-          </React.Fragment>
-        );
+      name: 'nama',
+      displayName: 'Peminjam',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.user.nama}</React.Fragment>;
       },
     },
     {
-      name: "quantity",
-      displayName: "Jumlah Dipinjam",
-      customRender: (rowData) => {
-        return (
-          <React.Fragment>
-            {rowData.quantity}
-          </React.Fragment>
-        );
+      name: 'quantity',
+      displayName: 'Jumlah Dipinjam',
+      customRender: rowData => {
+        return <React.Fragment>{rowData.quantity}</React.Fragment>;
       },
     },
     {
-      name: "status",
-      displayName: "Status"
+      name: 'status',
+      displayName: 'Status',
     },
     {
-      name: "actions",
-      displayName: "Actions",
-      customRender: (rowData) => {
+      name: 'actions',
+      displayName: 'Actions',
+      customRender: rowData => {
         return (
           <React.Fragment>
             <React.Fragment>
-              <button
-                className="bg-green-400 text-white active:bg-indigo-600 text-xs   px-3 py-1 rounded outline-none focus:outline-none "
-                type="button"
-                style={{ marginRight: '5px' }}
-                disabled={rowData.status === "Dikembalikan" ? true : false}
-              >
-                Return Ebook
-              </button>
+              {rowData.status !== 'Dikembalikan' ? (
+                <button
+                  className="bg-green-400 text-white active:bg-indigo-600 text-xs   px-3 py-1 rounded outline-none focus:outline-none "
+                  type="button"
+                  style={{ marginRight: '5px' }}
+                  onClick={() => returnEbook(rowData.id)}
+                  disabled={rowData.status === 'Dikembalikan' ? true : false}
+                >
+                  Return Ebook
+                </button>
+              ) : (
+                '-'
+              )}
             </React.Fragment>
           </React.Fragment>
         );
       },
-    }
-  ]
-
+    },
+  ];
 
   if (loading) return null;
   const { transactionEbooks } = props;
 
   return (
     <React.Fragment>
-      {transactionEbooks.data !== undefined ? <TableApproval columns={columns} source={transactionEbooks} isLoading={loading} limit={filterOptions.limit} page={filterOptions.page} onPaginationUpdated={onPaginationUpdated} /> : null}
+      {transactionEbooks.data !== undefined && transactionEbooks.data.length !== 0 ? (
+        <TableApproval
+          columns={columns}
+          source={transactionEbooks}
+          isLoading={loading}
+          limit={filterOptions.limit}
+          page={filterOptions.page}
+          onPaginationUpdated={onPaginationUpdated}
+        />
+      ) : (
+        <NoData msg="Belum ada request Dari user!" />
+      )}
     </React.Fragment>
   );
 }
