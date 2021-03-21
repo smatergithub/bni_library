6fc0175b8b8e8da6ff0bb4ea134fef3425780c39
hendra <gunawan4041@gmail.com>
2021-03-21 11:35:15 +0700

    deploy build

---
+++ react/build/precache-manifest.09d4663073472d37ce0592315d84ad0b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093f0d50076c2a5066c394177d41b93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "559011d8a9f1bbe799a0",
+    "url": "/static/css/2.032e083d.chunk.css"
+  },
+  {
+    "revision": "880cc42cf4f21192bef9",
+    "url": "/static/css/main.8cf5a523.chunk.css"
+  },
+  {
+    "revision": "559011d8a9f1bbe799a0",
+    "url": "/static/js/2.dff8b6b1.chunk.js"
+  },
+  {
+    "revision": "90e6c4f8074485259f6da3eac95fdbdc",
+    "url": "/static/js/2.dff8b6b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880cc42cf4f21192bef9",
+    "url": "/static/js/main.5d02c5b1.chunk.js"
+  },
+  {
+    "revision": "9a88d25d0502844295d0",
+    "url": "/static/js/runtime-main.7e40f857.js"
+  },
+  {
+    "revision": "96cada5015d3c8d7fd60927cc1f1aa10",
+    "url": "/static/media/NoImage.96cada50.png"
+  },
+  {
+    "revision": "b2ee8ce9c29fdd7f958d26952ab5dbcc",
+    "url": "/static/media/book.b2ee8ce9.svg"
+  },
+  {
+    "revision": "3af0f96f542bb5145a19016dcd8f8585",
+    "url": "/static/media/bookLover.3af0f96f.svg"
+  },
+  {
+    "revision": "c41f6908a60ff346d5162c0f13db92f2",
+    "url": "/static/media/box.c41f6908.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "6b881cd0159c6a65cd7beb54352e1d3c",
+    "url": "/static/media/building.6b881cd0.png"
+  },
+  {
+    "revision": "71b06f741f6edbb3b5b42f2b72e25daa",
+    "url": "/static/media/done.71b06f74.png"
+  },
+  {
+    "revision": "37851eb96e600cca46297d5c214b4793",
+    "url": "/static/media/ebook.37851eb9.svg"
+  },
+  {
+    "revision": "a55b2edc3e69f0218241c371299cb122",
+    "url": "/static/media/fasilitas.a55b2edc.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "15cae417ecc70f6689c8bf278fc95f88",
+    "url": "/static/media/katalog.15cae417.png"
+  },
+  {
+    "revision": "aa1d0084961ae1c21de6447ea6295a0f",
+    "url": "/static/media/newspaper.aa1d0084.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cc20d61e0a40169eeb7aae8344c76fe6",
+    "url": "/static/media/research.cc20d61e.png"
+  },
+  {
+    "revision": "1939c148d11b940b88faf69b17893e4a",
+    "url": "/static/media/riset.1939c148.png"
+  },
+  {
+    "revision": "936e568e17d94e17bfe1c100bc314e57",
+    "url": "/static/media/riset.936e568e.svg"
+  },
+  {
+    "revision": "3558e1111b7fed38908f9a1675e06164",
+    "url": "/static/media/town.3558e111.png"
+  }
+]);
