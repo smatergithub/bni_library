025b787e1e38686efb7a6c6062e883a083bdc59d
hendra <gunawan4041@gmail.com>
2021-07-21 14:45:37 +0700

    add frontend

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -6,12 +6,12 @@ const logger = createLogger({
     format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
   transports: [
-    new transports.File({
-      filename: './logs/all-logs.log',
-      json: false,
-      maxsize: 5242880,
-      maxFiles: 5,
-    }),
+    // new transports.File({
+    //   filename: './logs/all-logs.log',
+    //   json: false,
+    //   maxsize: 5242880,
+    //   maxFiles: 5,
+    // }),
     // new transports.Console(),
   ],
 });
